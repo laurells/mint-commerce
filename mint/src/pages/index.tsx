d646fd902e855f48bd4a7d8d7bf9ec88246f7bd1
Laurels Echichinnwo <laurels.echichinwo@stu.cu.edu.ng>
2023-12-08 11:14:50 +0000

    TopNav bar created

---
+++ mint/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header/Header";
+
+const Home = () => {
+  return (
+    <>
+      {/* ===== Header Section ===== */}
+      <Header />
+      <main id="main-content" className="-mt-20">
+      </main>
+    </>
+  )
+}
+
+export default Home;
